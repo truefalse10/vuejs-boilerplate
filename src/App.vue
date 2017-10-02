11c0dc4ae1f94b9c7f99ba23a57a611ce72eae2e
Sebastian Boolean <truefalse@posteo.de>
2017-10-02 11:17:54 +0200

    Changing Texts

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <h1>sebastian boolean</h1>
+    <h1>{{ message }}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis repellat quaerat, ut, incidunt culpa repudiandae accusamus dolorem obcaecati error corrupti vero, eveniet qui explicabo laborum possimus. Maiores minus molestias provident!</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  data() {
+    return {
+      message: 'Hello Vuejs',
+    };
+  },
 };
 </script>
 
@@ -19,5 +25,6 @@ $variable: #2c3e50;
   text-align: center;
   color: $variable;
   margin-top: 60px;
+  padding: 0 30px;
 }
 </style>
