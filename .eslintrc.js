56feb37241c4c004370cdb75b3983e98b1149254
Sebastian Boolean <truefalse@posteo.de>
2018-07-30 14:27:31 +0200

    remove linter rule "prefer-default-export", remove .vscode folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   rules: {
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
     'no-param-reassign': ['error', { props: false }],
     'func-names': 'off',
     'no-confusing-arrow': 'off',
